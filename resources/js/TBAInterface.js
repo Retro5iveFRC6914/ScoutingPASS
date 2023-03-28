7dca6e2bb986cf2c308f60e993e917be7030c1de
Johnny5 aka FRC Team 6914 <38083782+Retro5iveFRC6914@users.noreply.github.com>
2023-03-28 12:17:29 -0400

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "GiMCkJypdBmiMwRpdYEF1aAjauDU5tm07utTShBlEL0MCX7BuRRxIWsKgFq4Dyc2";
 /**
  * Get list of teams in event
  *
