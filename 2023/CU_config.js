2b8c5a87d507beb0b910326a8c38f5b5b4f56b8e
Johnny5 aka FRC Team 6914 <38083782+Retro5iveFRC6914@users.noreply.github.com>
2023-03-28 11:51:48 -0400

    Edited to FIM Livonia District

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mili",
+      "defaultValue": "2023 FIM Livionia District",
       "required": "true",
       "disabled": "true"
     },
