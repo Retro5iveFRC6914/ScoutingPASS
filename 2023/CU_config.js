3e710567ba33e299365ee72c4eba89e8e5aad41b
Johnny5 aka FRC Team 6914 <38083782+Retro5iveFRC6914@users.noreply.github.com>
2023-03-28 11:50:10 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023fimlivonia",
+      "defaultValue": "2023mili",
       "required": "true",
       "disabled": "true"
     },
